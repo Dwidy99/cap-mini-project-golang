f1de4d967ea392a9809670ccb7d80d316eff9bfb
Dwidy99 <98dwiyulianto@gmail.com>
2022-08-28 23:27:12 +0700

    update json output

--- campaign/campaignEntity.go
+++ campaign/campaignEntity.go
@@ -7,25 +7,25 @@ import (
 type Campaign struct {
 	ID               int `json:"campaign_id" gorm:"column:campaign_id"`
 	UserID           int `json:"user_id" gorm:"column:user_id"`
-	CampaignName             string
-	ShortDescription string
-	Description string
-	Perks            string
+	CampaignName             string `json:"campaign_name"`
+	ShortDescription string `json:"short_description"`
+	Description string `json:"description"`
+	Perks            string `json:"perks"`
 	BeckerCount      int `json:"backer_count" gorm:"column:backer_count"`
-	GoalAmount       int
-	CurrentAmount    int
-	Slug             string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
-	CampaignImages []CampaignImage
+	GoalAmount       int `json:"goal_amount"`
+	CurrentAmount    int `json:"current_amount"`
+	Slug             string `json:"slug"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+	CampaignImages []CampaignImage `json:"campaign_images"`
 	// User user.User `json:"user"`
 }
 
 type CampaignImage struct {
 	ID int `json:"id" gorm:"column:campaign_image_id"`
 	CampaignID int `json:"campaign_id" gorm:"column:campaign_id"`
-	FileName string
-	IsPrimary int
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	FileName string `json:"file_name"`
+	IsPrimary int `json:"is_primary"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
--- user/userEntity.go
+++ user/userEntity.go
@@ -5,12 +5,12 @@ import "time"
 type User struct {
 	ID           int `json:"user_id" gorm:"column:user_id"`
 	Name              string `json:"name" gorm:"column:name"`
-	Occupasion        string
-	Email             string
-	Password_hash     string
-	AvatarFieldName string
-	Role              string
-	Token              string
-	CreatedAt         time.Time
-	UpdatedAt         time.Time
+	Occupasion        string `json:"occupasion"`
+	Email             string `json:"email"`
+	Password_hash     string `json:"password_hash"`
+	AvatarFieldName string `json:"avatar_field_name"`
+	Role              string `json:"role"`
+	Token              string `json:"token"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
 }
